acbe4c79e612a82c9e93f9cb5d69fb0391ae6c07
Oleg.Nee <89423263+olegnee@users.noreply.github.com>
2022-03-13 03:40:27 +0900

    edited homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,20 +18,22 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        Stream<Person> stream = persons.stream();
-        stream.
+        long minorPopulation = persons.stream().
                 filter(x -> x.getAge() < 18).
                 count();
-        Stream<Person> stream1 = persons.stream();
-        stream1.
-                filter(x -> x.getAge() >= 18 && x.getAge() <= 27).
+
+        List<String> recruitablePopulation = persons.stream().
+                filter(x -> x.getAge() >= 18).
+                filter(x -> x.getAge() <= 27).
                 map(x -> x.getFamily()).
                 collect(Collectors.toList());
-        Stream<Person> stream2 = persons.stream();
-        stream2.
+
+        List<Person> workablePopulation = persons.stream().
                 filter(x -> x.getEducation() == Education.HIGHER).
-                filter(x -> x.getSex() == Sex.WOMAN && x.getAge() >= 18 && x.getAge() <= 60).
-                filter(x -> x.getSex() == Sex.MAN && x.getAge() >= 18 && x.getAge() <= 65).
+                filter(x -> x.getSex() == Sex.WOMAN && x.getAge() >= 18).
+                filter(x -> x.getSex() == Sex.WOMAN && x.getAge() <= 60).
+                filter(x -> x.getSex() == Sex.MAN && x.getAge() >= 18).
+                filter(x -> x.getSex() == Sex.MAN && x.getAge() <= 65).
                 sorted(Comparator.comparing(x -> x.getFamily())).
                 collect(Collectors.toList());
     }
